42d067b3f109ac336ff21e6f8ddc00862ea2a4b9
hiisuya <hiisuya@gmail.com>
2025-12-02 16:02:47 -0600

    day 2 part 2

--- day-02/day-02.cpp
+++ day-02/day-02.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 size_t get_invalid_ids(string filePath);
+size_t get_all_repeat_ids(string filePath);
 
 int main() {
     cout << get_invalid_ids("input.txt") << "\n";
+    cout << get_all_repeat_ids("input.txt") << "\n";
 }
 
 vector<string> split_string_by(string input, string delimiter) {
@@ -21,6 +24,15 @@ vector<string> split_string_by(string input, string delimiter) {
     }
 
     output.push_back(input);
+
+    return output;
+}
+
+string convert_vector_to_string(vector<string> input) {
+    string output;
+    for (string i : input) {
+        output.append(i);
+    }
 
     return output;
 }
@@ -43,10 +55,75 @@ size_t get_invalid_ids(string filePath) {
             size_t len = input.length();
             string front = input.substr(0, len / 2);
             string back = input.substr((len / 2), -1);
-            
+
             if (len >= 2) {
                 if (front.compare(back) == 0) {
                     total += index;
+                }
+            }
+        }
+    }
+
+    return total;
+}
+
+size_t get_all_repeat_ids(string filePath) {
+    ifstream t(filePath);
+    string str((istreambuf_iterator<char>(t)),
+                istreambuf_iterator<char>());
+
+    vector<string> inputs = split_string_by(str, ",");
+
+    size_t total = 0;
+    for (int i = 0; i < inputs.size(); i++) {
+        vector<string> nums = split_string_by(inputs[i], "-");
+        size_t num1 = std::stol(nums[0]);
+        size_t num2 = std::stol(nums[1]);
+
+        for (size_t index = num1; index <= num2; index++) {
+            string input = to_string(index);
+            size_t len = input.length();
+            vector<string> pattern = {};
+
+            for (int k = 0; k < len; k++) {
+                int p_len = pattern.size();
+                
+                if (p_len == 0) {
+                    pattern.push_back(input.substr(k, 1));
+                    continue;
+                }
+
+                string p = convert_vector_to_string(pattern);
+
+                if (p.compare(input.substr(k, p_len)) == 0) {
+
+                    if (len == 2) {
+                        total += index;
+                        break;
+                    }
+
+                    bool matching = false;
+                    int pos = p_len;
+
+                    while (pos <= len) {
+                        if (p.compare(input.substr(pos, p_len)) == 0) {
+                            pos += p_len;
+                            if (pos == len) {
+                                matching = true;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                    
+                    if (matching) {
+                        total += index;
+                        break;
+                    } else {
+                        pattern.push_back(input.substr(k, 1));
+                    }
+                } else {
+                    pattern.push_back(input.substr(k, 1));
                 }
             }
         }
