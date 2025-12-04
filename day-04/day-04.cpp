9ffb0481c8d08689ca05410e0224b5509d275134
hiisuya <hiisuya@gmail.com>
2025-12-04 11:59:31 -0600

    day 4 part 2

--- day-04/day-04.cpp
+++ day-04/day-04.cpp
@@ -8,9 +8,11 @@
 using namespace std;
 
 size_t get_forklift_roll_access(string filePath);
+size_t get_forklift_roll_access_max(string filePath);
 
 int main() {
     cout << get_forklift_roll_access("input.txt") << "\n";
+    cout << get_forklift_roll_access_max("input.txt") << "\n";
 }
 
 size_t get_forklift_roll_access(string filePath) {
@@ -34,7 +36,7 @@ size_t get_forklift_roll_access(string filePath) {
 
         for (int row_index = 0; row_index < grid[row].size(); row_index++) {
             int paper = 0;
-            if (grid[row][row_index] == '.' || paper >= MAX_PAPER) {
+            if (grid[row][row_index] == '.') {
                 continue;
             }
 
@@ -59,6 +61,72 @@ size_t get_forklift_roll_access(string filePath) {
             }
         }
     }
+
+    return total;
+}
+
+size_t get_forklift_roll_access_max(string filePath) {
+    ifstream input(filePath);
+
+    vector<vector<char>> grid;
+
+    for(string line; getline( input, line );) {
+        vector<char> output(line.length());
+        for (int i = 0; i < line.length(); i++) {
+            output[i] = line[i];
+        }
+        grid.push_back(output);
+    }
+
+    size_t total = 0;
+    bool done = false;
+    while (!done) {
+        vector<pair<int, int>> remove_indeces;
+        
+        for (int row = 0; row < grid.size(); row++) {
+            const int MIN_CHECK = -1;
+            const int MAX_CHECK = 1;
+            const int MAX_PAPER = 4;
+
+            for (int row_index = 0; row_index < grid[row].size(); row_index++) {
+                int paper = 0;
+                if (grid[row][row_index] == '.') {
+                    continue;
+                }
+
+                for (int x = MIN_CHECK; x <= MAX_CHECK; x++) {
+                    if ((row_index + x) <= MIN_CHECK || (row_index + x) > (grid[row].size() - 1)) {
+                        continue;
+                    }
+
+                    for (int y = MIN_CHECK; y <= MAX_CHECK; y++) {
+                        if ((row + y <= MIN_CHECK) || ((row + y) > (grid.size() - 1)) || (x == 0 && y == 0)) {
+                            continue;
+                        }
+
+                        if (grid[row + y][row_index + x] == '@') {
+                            paper++;
+                        }
+                    }
+                }
+
+                if (paper < MAX_PAPER) {
+                    remove_indeces.push_back({row_index, row});
+                    total++;
+                }
+            }
+        }
+
+        if (remove_indeces.size() == 0) {
+            done = true;
+            break;
+        } else {
+            for (pair<int, int> v: remove_indeces) {
+                grid[v.second][v.first] = '.';
+            }
+        }
+    }
+    
 
     return total;
 }
