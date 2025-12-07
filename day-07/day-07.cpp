317142a122c1f34eff2485e08a2df74480f147cd
hiisuya <hiisuya@gmail.com>
2025-12-07 14:54:08 -0600

    part 1 cleanup

--- day-07/day-07.cpp
+++ day-07/day-07.cpp
@@ -20,9 +20,7 @@ size_t get_splits(const string& filePath) {
     size_t total = 0;
     for (int i = 0; i < lines.size(); i++) {
         if (lines[i].find_first_of("S") != string::npos) {
-            int start_pos = lines[i].find_first_of("S");
-            beam_pos.push_back(start_pos);
-            lines[i + 1][start_pos] = '|';
+            beam_pos.push_back(lines[i].find_first_of("S"));
             continue;
         }
 
@@ -30,20 +28,16 @@ size_t get_splits(const string& filePath) {
         for (int beam = 0; beam < beam_pos.size(); beam++) {
             int pos = beam_pos[beam];
             if (lines[i][pos] == '^') {
-                lines[i][pos - 1] = '|';
-                lines[i][pos + 1] = '|';
                 temp.push_back(pos - 1);
                 temp.push_back(pos + 1);
                 total += 1;
             } else {
-                lines[i][pos] = '|';
                 if (count(temp.begin(), temp.end(), pos) == 0) {
                     temp.push_back(pos);
                 }
             }
         }
-
-        beam_pos.clear();
+        
         beam_pos = temp;
     }
 
